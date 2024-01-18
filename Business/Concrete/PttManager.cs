589115941be7bedf890215591ff565b658b7ea15
Oguzhan <Oguzhan@DESKTOP-DMI9A65>
2024-01-18 17:17:30 +0300

    Add project files.

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    //SONAR QUBE Software Code Quality Tool
+    public class PttManager: ISupplierService 
+    {
+        private IApplicantService _applicantService;
+
+        //Constructor
+        public PttManager(IApplicantService applicantService)
+        {
+            _applicantService = applicantService;
+        }
+
+        public void GiveMask(Person person)
+        {
+            string fullName = person.FirstName + " " + person.LastName;
+
+            Console.WriteLine(fullName + " için MERNIS SORGULAMA BAŞLATILDI...");
+
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(fullName + " TCKN Doğrulandı");
+            }
+            else
+            {
+                Console.WriteLine(fullName + " TCKN Doğrulama BAŞARISIZ!");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person = new Person();
+
+            person.NationalIdentity = 12345678901;
+            person.FirstName = "AHMET";
+            person.LastName = "TURP";
+            person.DateOfBirthYear = 2000;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person);
+
+            Console.ReadLine();
+        }
+
+    }
+}
